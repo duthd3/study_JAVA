4f340e3b0db14e4ae296c435e4b9e709c7a135a7
user name <79510152+duthd3@users.noreply.github.com>
2021-03-09 23:59:10 +0900

    Create CallStackTest.java

---
+++ 21_03_09/CallStackTest.java
@@ -0,0 +1,17 @@
+public class CallStackTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		firstMethod();
+
+	}
+	
+	static void firstMethod() {
+		secondMethod();
+	}
+	
+	static void secondMethod() {
+		System.out.println("secondMethod()");
+	}
+
+}
