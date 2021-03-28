8b1ce8f9b5fd95c6228ed568c214c24c2f7c51dc
user name <duthd3@naver.com>
2021-03-28 21:55:45 +0900

    Create MyParent.java

---
+++ 21_03_28/MyParent.java
@@ -0,0 +1,27 @@
+package pkg1;
+
+public class MyParent{ //
+	private int prv; //같은 클래스
+	int dft;// 같은 패키지
+	protected int prt;//같은 패키지 + 자손(다른 패키지)
+	public int pub;//접근제한 없음.
+	
+	public void printMembers() {
+		System.out.println(prv);
+		System.out.println(dft);
+		System.out.println(prt);
+		System.out.println(pub);
+	}
+}
+class MyParentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyParent p = new MyParent();
+//		System.out.println(p.prv); //접근범위가 같은 클래스
+		System.out.println(p.dft);
+		System.out.println(p.prt);
+		System.out.println(p.pub);
+	}
+
+}
