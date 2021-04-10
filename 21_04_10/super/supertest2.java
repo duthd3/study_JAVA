19f6947c806c8e16914d27bc4f5a4cbe64ca2484
YeoSong <duthd3@naver.com>
2021-04-10 19:41:40 +0900

    Create supertest2.java

---
+++ 21_04_10/super/supertest2.java
@@ -0,0 +1,25 @@
+
+public class SuperTest2 {
+
+	public static void main(String[] args) {		
+		// TODO Auto-generated method stub
+		Child c = new Child();
+		c.method();
+
+	}
+
+}
+
+class Parent{
+	int x=10;
+}
+
+class Child extends Parent{
+	int x=20;
+	
+	void method() {
+		System.out.println("x=" +x);
+		System.out.println("this.x=" + this.x);//자식클래스의 x
+		System.out.println("super.x =" + super.x);//조상클래스의 x
+	}
+}
