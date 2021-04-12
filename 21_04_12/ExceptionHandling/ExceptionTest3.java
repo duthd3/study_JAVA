59b9e0183bc61249c3a273cc2ed240496e3689a6
YeoSong <duthd3@naver.com>
2021-04-12 20:09:47 +0900

    Create ExceptionTest3.java

---
+++ 21_04_12/ExceptionHandling/ExceptionTest3.java
@@ -0,0 +1,24 @@
+public class ExceptionTest3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		System.out.println(2);
+		try {
+			System.out.println(3);
+			System.out.println(args[0]);
+			System.out.println(4);
+		}catch(ArithmeticException ae) {
+			if(ae instanceof ArithmeticException)
+				System.out.println("true");
+			System.out.println("ArithmeticException");
+		}catch(Exception e) {
+			System.out.println("AIOOBE");
+		}
+		System.out.println(6);
+		
+		
+
+	}
+
+}
