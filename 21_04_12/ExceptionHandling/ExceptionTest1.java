a394cc9543ac3ef06715184fa081e240cecd6afc
YeoSong <duthd3@naver.com>
2021-04-12 20:09:28 +0900

    Create ExceptionTest1.java

---
+++ 21_04_12/ExceptionHandling/ExceptionTest1.java
@@ -0,0 +1,16 @@
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		try {
+			System.out.println(2);
+			System.out.println(3);
+		}catch(Exception e) {
+			System.out.println(4);
+		}
+		System.out.println(5);
+	}
+
+}
