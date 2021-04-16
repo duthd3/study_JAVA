4aac59a326d7b03db1f5e47364c309e4305c6d02
YeoSong <duthd3@naver.com>
2021-04-17 03:22:43 +0900

    Create ExceptionEx5.java

---
+++ ExceptionHandling/ExceptionEx5.java
@@ -0,0 +1,19 @@
+
+public class ExceptionEx5{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		System.out.println(2);
+		try {
+			System.out.println(3);
+			System.out.println(0/0);//예외 발생!
+			System.out.println(4);
+		}catch(ArithmeticException ae) {
+			System.out.println(5); //예외가 발생했으므로 실행
+		}
+		System.out.println(6);
+
+	}
+
+}
