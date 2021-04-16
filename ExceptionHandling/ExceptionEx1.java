f7d96b6759283094c0664ea060b22768b4cb7535
YeoSong <duthd3@naver.com>
2021-04-17 03:21:57 +0900

    Create ExceptionEx1.java

---
+++ ExceptionHandling/ExceptionEx1.java
@@ -0,0 +1,19 @@
+
+public class ExceptionEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			try { } catch (Exception e) {}
+		}catch(Exception e) {
+			try {}catch(Exception x) {}
+		}
+
+		try {
+			
+		}catch(Exception e) {
+			
+		}
+	}
+
+}
