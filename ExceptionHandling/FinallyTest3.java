67c1b2891e04026b10e70c07a0c69f848a731847
YeoSong <duthd3@naver.com>
2021-04-17 21:47:02 +0900

    Create FinallyTest3.java

---
+++ ExceptionHandling/FinallyTest3.java
@@ -0,0 +1,22 @@
+
+public class FinallyTest3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FinallyTest3.method1();
+		System.out.println("method1()의 수행을 마치고 main메서드로 돌아왔습니다.");
+	}
+	
+	static void method1() {
+		try {
+			System.out.println("method1()이 호출되었습니다.");
+			return;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			System.out.println("method1()의 finally블럭이 실행되었습니다.");
+		}
+		
+	}
+
+}
