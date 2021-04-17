0d459f9236f16c0acd4d666f5bf27d08a0cb93ce
YeoSong <duthd3@naver.com>
2021-04-18 00:09:21 +0900

    Create ToStringTest.java

---
+++ java.langPackage/ToStringTest.java
@@ -0,0 +1,16 @@
+
+public class ToStringTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = new String("KOREA");
+		java.util.Date today = new java.util.Date();
+		
+		System.out.println(str);
+		System.out.println(str.toString());
+		System.out.println(today);
+		System.out.println(today.toString());
+
+	}
+
+}
