ff454fe7052a6a0269ca06bee0ffb469b98d8994
YeoSong <duthd3@naver.com>
2021-04-19 21:30:13 +0900

    Create CloneEx2.java

---
+++ java.langPackage/Clone()/CloneEx2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class CloneEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1,2,3,4,5};
+		int[] arrClone = arr.clone();
+		arrClone[0] = 6;
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arrClone));
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
