4bb8d9ce79dfd3046917c098ae85b799ff31f530
YeoSong <duthd3@naver.com>
2021-04-20 22:30:34 +0900

    Create StringEx6.java

---
+++ java.langPackage/StringClass/StringEx6.java
@@ -0,0 +1,20 @@
+
+public class StringEx6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int iVal = 100;
+		String strVal = String.valueOf(iVal); //int를 String으로 변환한다.
+		
+		double dVal = 200.0;
+		String strVal2 = dVal + ""; 
+		
+		double sum = Integer.parseInt("+" + strVal) + Double.parseDouble(strVal2);
+		double sum2 = Integer.valueOf(strVal) + Double.valueOf(strVal2);
+		
+		System.out.println(String.join("", strVal, "+", strVal2, "=") +sum);
+		System.out.println(strVal + "+" + strVal2 + "=" + sum2);
+
+	}
+
+}
