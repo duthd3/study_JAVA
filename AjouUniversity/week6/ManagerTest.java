10493ff071ac6127a5588dde97e89a9ec7a8332d
YeoSong <duthd3@naver.com>
2021-04-20 21:03:16 +0900

    Create ManagerTest.java

---
+++ AjouUniversity/week6/ManagerTest.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class ManagerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee harry = new Employee("Harry Hacker", 50000, 1989 , 10, 1);
+		System.out.println(harry);
+	}
+
+}
