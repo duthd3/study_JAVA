ac32121366aa40603644f2d2e4415541b3c84608
YeoSong <duthd3@naver.com>
2021-04-20 22:30:06 +0900

    Create StringEx3.java

---
+++ java.langPackage/StringClass/StringEx3.java
@@ -0,0 +1,14 @@
+
+public class StringEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] cArr = new char[0];
+		String s = new String(cArr);
+		
+		System.out.println("cArr.length =" + cArr.length);
+		System.out.println("@@@" + s + "@@@");
+
+	}
+
+}
