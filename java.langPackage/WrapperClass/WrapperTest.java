7337bdc1131af0d4a99a4b8ef6ee5367979ef06d
YeoSong <duthd3@naver.com>
2021-04-26 23:54:33 +0900

    Create WrapperTest.java

---
+++ java.langPackage/WrapperClass/WrapperTest.java
@@ -0,0 +1,23 @@
+
+public class Ex9_14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i = new Integer(100);
+		Integer i2 = new Integer(100);
+		
+		System.out.println("i==12? " +(i==12));
+		System.out.println("i.equals(i2)? " +i.equals(i2));
+		System.out.println("i.compareTo(i2)? " +i.compareTo(i2));		
+		System.out.println("i.toString(=? " +i.toString());
+
+		System.out.println("Max_Value= " +Integer.MAX_VALUE);
+		System.out.println("Min_Value= " +Integer.MIN_VALUE);
+		System.out.println("Size= " +Integer.SIZE + "bits");
+		System.out.println("BYTES= " +Integer.BYTES + "bytes");
+		System.out.println("TYPE= " +Integer.TYPE);
+		
+		
+	}
+
+}
