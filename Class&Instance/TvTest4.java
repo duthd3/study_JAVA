a1c4c43120761717b6c624937287fdb82350905e
YeoSong <duthd3@naver.com>
2021-04-28 01:18:56 +0900

    Create TvTest4.java

---
+++ Class&Instance/TvTest4.java
@@ -0,0 +1,18 @@
+
+public class TvTest4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tv[] tvArr = new Tv[3];
+		
+		for(int i=0; i<tvArr.length; i++) {
+			tvArr[i] = new Tv();
+			tvArr[i].channel = i+10;
+		}
+		for(int i=0; i<tvArr.length; i++) {
+			tvArr[i].channelUp();
+			System.out.printf("tvArr[%d].channel=%d%n", i, tvArr[i].channel);
+		}
+	}
+
+}
