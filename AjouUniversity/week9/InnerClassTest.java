6d67cf2aa88fa547ce04ec45139f2671dc0b14d4
YeoSong <duthd3@naver.com>
2021-04-29 02:55:47 +0900

    Create InnerClassTest.java

---
+++ AjouUniversity/week9/InnerClassTest.java
@@ -0,0 +1,40 @@
+package week9;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class InnerClassTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TalkingClock clock = new TalkingClock(1000,true);
+		clock.start();
+		JOptionPane.showMessageDialog(null, "Quit Program?");
+		System.exit(0);
+
+	}
+
+}
+class TalkingClock{
+	private int interval;
+	private boolean beep;
+	public TalkingClock(int interval, boolean beep) {
+		this.interval = interval;
+		this.beep = beep;
+	}
+	public void start()
+	{
+		ActionListener listener = new TimePrinter();
+		Timer t = new Timer(interval, listener);
+		t.start();
+				
+	}
+	public class TimePrinter implements ActionListener{
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("At the tone, the time is" + new Date());
+		if(beep)Toolkit.getDefaultToolkit().beep();
+		}
+	}
+}
