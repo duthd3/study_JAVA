580cf99745b3994d07a781bbfc9f70468f7fdb09
YeoSong <duthd3@naver.com>
2021-05-02 14:11:58 +0900

    Create ReferenceParamEx.java

---
+++ Variables&Method/ReferenceParamEx.java
@@ -0,0 +1,23 @@
+class Data2{int x;}
+public class ReferenceParamEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Data2 d = new Data2();
+		d.x = 10;
+		System.out.println("main() : x= " + d.x);
+		
+		change(d);
+		System.out.println("After change(d)");
+		System.out.println("main() : x=" + d.x);
+		
+		
+
+	}
+	
+	static void change(Data2 d) {
+		d.x = 1000;
+		System.out.println("change(): x=" + d.x);
+	}
+
+}
