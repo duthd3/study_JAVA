d3d099212c1765348d9808feb6a6425bb062c5e5
YeoSong <duthd3@naver.com>
2021-05-02 14:12:33 +0900

    Create ReferenceReturnEx.java

---
+++ Variables&Method/ReferenceReturnEx.java
@@ -0,0 +1,21 @@
+class Data3{int x;}
+public class ReferenceReturnEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Data3 d = new Data3();
+		d.x = 10;
+		
+		Data3 d2 = copy(d);
+		System.out.println("d.x = "+ d.x);
+		System.out.println("d2.x = " + d2.x);
+
+	}
+	static Data3 copy(Data3 d) {
+		Data3 tmp = new Data3();
+		tmp.x = d.x;
+		return tmp;
+		
+	}
+
+}
