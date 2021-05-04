e78ba97c452904b543e79b950d27432f21ce06c8
YeoSong <duthd3@naver.com>
2021-05-05 03:49:11 +0900

    Create StaticBlockTest.java

---
+++ VariableInitialization/StaticBlockTest.java
@@ -0,0 +1,17 @@
+
+public class StaticBlockTest {
+	static int[] arr = new int[10];
+	
+	static {
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = (int)(Math.random()*10) +1;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<arr.length; i++) {
+			System.out.println("arr["+i+"] :" + arr[i]);
+		}
+	}
+
+}
