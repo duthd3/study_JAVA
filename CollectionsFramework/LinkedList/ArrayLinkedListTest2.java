6270c9a45d50befb40eab2582026efb49d62c2fc
YeoSong <duthd3@naver.com>
2021-05-13 21:52:48 +0900

    Create ArrayLinkedListTest2.java

---
+++ CollectionsFramework/LinkedList/ArrayLinkedListTest2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class ArrayListLinkedListTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList al = new ArrayList(100000);
+		LinkedList ll = new LinkedList();
+		add(al);
+		add(ll);
+		
+		System.out.println("=접근시간테스트=");
+		System.out.println("ArrayList:" + access(al));
+		System.out.println("LinkedList:" + access(ll));
+		
+	}
+	
+	public static void add(List list) {
+		for(int i=0; i<100000; i++) {
+			list.add(i+"");
+		}
+	}
+	
+	public static long access(List list) {
+		long start = System.currentTimeMillis();
+		for(int i=0; i<10000; i++) {
+			list.get(i);
+		}
+		long end = System.currentTimeMillis();
+		return end-start;
+	}
+
+}
