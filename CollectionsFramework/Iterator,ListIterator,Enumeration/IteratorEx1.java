adb0f12c7f352b6273ac5987a8e89abca06b7037
YeoSong <duthd3@naver.com>
2021-05-14 02:27:47 +0900

    Create IteratorEx1.java

---
+++ CollectionsFramework/Iterator,ListIterator,Enumeration/IteratorEx1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class IteratorEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList list = new ArrayList();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+		
+		Iterator it = list.iterator();
+		while(it.hasNext()) {
+		//	Object obj = it.next();
+			System.out.println(it.next());
+		}
+	}
+
+}
