4d4c09102e97d6ccaca514969ddeea70f882c9ac
YeoSong <duthd3@naver.com>
2021-05-19 23:50:36 +0900

    Create SizedFrameTest.java

---
+++ AjouUniversity/week12/SizedFrameTest.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class SizedFrameTest {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(()->{
+			JFrame frame = new SizedFrame();
+			frame.setTitle("SizedFrame");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+		});
+	}
+
+}
+
+class SizedFrame extends JFrame{
+	public SizedFrame() {
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		
+		setSize(screenWidth / 2, screenHeight/ 2);
+		setLocationByPlatform(true);
+		
+		//set frame icon
+		Image img = new ImageIcon("icon.gif").getImage();
+		setIconImage(img);
+	}
+}
