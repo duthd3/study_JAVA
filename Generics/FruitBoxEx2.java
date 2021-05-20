632d55c3406a6eff3044edecb6ceab7d175274a7
YeoSong <duthd3@naver.com>
2021-05-20 21:36:07 +0900

    Create FruitBoxEx2.java

---
+++ Generics/FruitBoxEx2.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+class Fruit implements Eatable{
+	public String toString() {return "Fruit";}
+}
+
+class Apple extends Fruit{
+	public String toString() {return "Apple";}
+}
+
+class Grape extends Fruit{
+	public String toString() {return "Grape";}
+}
+
+class Toy{
+	public String toStrint() {return "Toy";}
+	
+}
+
+interface Eatable{}
+public class FruitBoxEx2 {
+
+	public static void main(String[] args) {
+		FruitBox<Fruit> fruitBox = new FruitBox<Fruit>();
+		FruitBox<Apple> appleBox = new FruitBox<Apple>();
+		FruitBox<Grape> grapeBox = new FruitBox<Grape>();
+		
+		fruitBox.add(new Fruit());
+		fruitBox.add(new Apple());
+		fruitBox.add(new Grape());
+		
+		appleBox.add(new Apple());
+		
+		grapeBox.add(new Grape());
+		
+		System.out.println("fruitBox-" + fruitBox);
+		System.out.println("appleBox-" + appleBox);
+		System.out.println("grapeBox-" + grapeBox);
+	}
+
+}
+
+class FruitBox<T extends Fruit & Eatable> extends Box<T>{}
+class Box<T>{
+	ArrayList<T> list = new ArrayList<T>();
+	void add(T item) {
+		list.add(item);
+	}
+	T get(int i)
+	{
+		return list.get(i);
+	}
+	int size() {
+		return list.size();
+	}
+	public String toString() {
+		return list.toString();
+	}
+}
