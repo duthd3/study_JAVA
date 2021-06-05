ad74f39cd773099fcfb75acc760bd93838bfa458
YeoSong <duthd3@naver.com>
2021-06-05 23:25:36 +0900

    Update 02.java

--- AlgorithmBase/02.java
+++ AlgorithmBase/02.java
@@ -3,13 +3,13 @@
 public class Main2 {
 
 	public static void main(String[] args) {
-		int[] arr = new int[100];
+		int[] arr = new int[10];
 		arr[0] = 1;
 		arr[1] = 1;
 		
 		System.out.print(arr[0]+" ");
 		System.out.print(arr[1]+" ");
-		for(int i=2; i<arr.length; i++) {
+		for(int i=2; i<10; i++) {
 			arr[i] = arr[i-1]+arr[i-2];
 			System.out.print(arr[i]+" ");
 		}
