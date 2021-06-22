d527c5ba5f98485cb8a3fb8d06d8b717b86626e2
YeoSong <duthd3@naver.com>
2021-06-23 02:30:10 +0900

    Add files via upload

---
+++ Thread/ThreadEx9.java
@@ -0,0 +1,30 @@
+
+public class ThreadEx9 {
+
+	public static void main(String[] args) throws Exception{
+		ThreadGroup main = Thread.currentThread().getThreadGroup();
+		ThreadGroup grp1 = new ThreadGroup("Group1");
+		ThreadGroup grp2 = new ThreadGroup("Group2");
+		
+		ThreadGroup subGrp1 = new ThreadGroup(grp1, "SubGroup1");
+		grp1.setMaxPriority(3); //쓰레드 그룹 grp1의 최대우선순위를 3으로 변경
+		
+		Runnable r = new Runnable() {
+			public void run() {
+				try {
+					Thread.sleep(1000);
+				}catch(InterruptedException e) {}
+			}
+		};
+		
+		new Thread(grp1, r, "th1").start();
+		new Thread(subGrp1, r, "th2").start();
+		new Thread(grp2, r, "th3").start();
+		
+		System.out.println(">>List of ThreadGroup: " + main.getName()
+		+ ", Active ThreadGroup: " + main.activeGroupCount() + ", Active Thread: " + main.activeCount());
+		main.list();
+	}
+	
+
+}
