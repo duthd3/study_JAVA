696d3e8be7dd7968d9c0b15a651df380803e90ba
YeoSong <duthd3@naver.com>
2021-06-26 03:10:24 +0900

    Create ThreadEx13.java

---
+++ Thread/ThreadEx13.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class ThreadEx13 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		ThreadEx13_1 th1 = new ThreadEx13_1();
+		th1.start();
+		String input = JOptionPane.showInputDialog("아무 값이나 입력하세요");
+		System.out.println("입력하신 값은" + input + "입니다.");
+		th1.interrupt();
+		System.out.println("isInterrupted(): " + th1.isInterrupted());
+	}
+
+}
+
+class ThreadEx13_1 extends Thread{
+	public void run() {
+	int i = 10;
+	while(i!=0 && !isInterrupted()) {
+		System.out.println(i--);
+		for(long x=0; x<2500000000L; x++);
+	}
+	System.out.println("카운트가 종료되었습니다.");
+	}
+}
