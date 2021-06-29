6d426b46d7f718b01ea9095de800946347f7856b
YeoSong <duthd3@naver.com>
2021-06-30 01:05:49 +0900

    Add files via upload

---
+++ Thread/ThreadEx19.java
@@ -0,0 +1,36 @@
+
+public class ThreadEx19 {
+	static long startTime = 0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThreadEx19_1 th1 = new ThreadEx19_1();
+		ThreadEx19_2 th2 = new ThreadEx19_2();
+		th1.start();
+		th2.start();
+		startTime = System.currentTimeMillis();
+		try {
+			th1.join();//main쓰레드가 th1의 작업이 끝날때 까지 기다린다.
+			th2.join();//main쓰레드가 th2의 작업이 끝날때 까지 기다린다.
+		}catch(InterruptedException e) {}
+		
+		System.out.println("소요시간: " + (System.currentTimeMillis()
+				- ThreadEx19.startTime));
+	}
+
+}
+
+class ThreadEx19_1 extends Thread{
+	public void run() {
+		for(int i=0; i<300; i++) {
+			System.out.print(new String("-"));
+		}
+	}
+}
+
+class ThreadEx19_2 extends Thread{
+	public void run() {
+		for(int i=0; i<300; i++) {
+			System.out.print(new String("|"));
+		}
+	}
+}
