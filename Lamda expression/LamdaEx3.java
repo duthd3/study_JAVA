a11fb779a3f6e85e33f8489003320cc46ca34b5b
YeoSong <duthd3@naver.com>
2021-07-09 00:35:27 +0900

    Create LamdaEx3.java

---
+++ Lamda expression/LamdaEx3.java
@@ -0,0 +1,36 @@
+@FunctionalInterface
+interface MyFunction{
+	void myMethod();
+}
+
+class Outer{
+	int val=10;
+	
+	class Inner{
+		int val=20;
+		
+		void method(int i) {
+			int val = 30;
+			
+			MyFunction f = () -> {
+				System.out.println("i=" + i);
+				System.out.println("val=" + val);
+				System.out.println("this.val=" + this.val);
+				System.out.println("Outer.this.val=" + Outer.this.val);
+			};
+			f.myMethod();
+		}
+	}
+}
+
+
+public class LamdaEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Outer outer = new Outer();
+		Outer.Inner inner = outer.new Inner();
+		inner.method(100);
+	}
+
+}
